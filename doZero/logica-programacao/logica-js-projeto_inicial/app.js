5dd9272506e2e883d9c8b6e91b1f1e36efe80e72
Lucas <lucas.emanuel.carvalho@outlook.com>
2025-06-18 19:53:58 -0300

    Adicionando counter

--- doZero/logica-programacao/logica-js-projeto_inicial/app.js
+++ doZero/logica-programacao/logica-js-projeto_inicial/app.js
@@ -1,19 +1,21 @@
 alert('Bem Vindo ao Jogo do Número Secreto');
 let numeroSecreto = 5;
-let guess
+let guess;
+// Iniciando o contador
+let counter = 1;
 
 // Criando laço enquanto (while)
-
 while (guess != numeroSecreto) {
     guess = prompt('Escolha um numero entre 1 e 10');
     // Criando a comparação/condição
     if (guess == numeroSecreto) {
-        alert(`Você acertou. O numero secreto é ${numeroSecreto}`);
+        alert(`Você acertou. O numero secreto é ${numeroSecreto}, com ${counter} tentativas`);
     } else {
         if (guess > numeroSecreto) {
-            alert(`O numero secreto é menor que ${guess}`)
+            alert(`O numero secreto é menor que ${guess}`);
         } else {
-            alert(`O numero secreto é maior que ${guess}`)
+            alert(`O numero secreto é maior que ${guess}`);
         }
+        counter++;
     }
 }
