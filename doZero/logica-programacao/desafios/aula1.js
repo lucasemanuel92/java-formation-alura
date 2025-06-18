d5c2e75e36c7afbe9e063d3d84b39ea88a2f566a
Lucas <lucas.emanuel.carvalho@outlook.com>
2025-06-18 18:30:58 -0300

    melhoria do programa e desafios aula 01

---
+++ doZero/logica-programacao/desafios/aula1.js
@@ -0,0 +1,36 @@
+// Resolução dos desafios da aula 01.Iniciando com JavaScript
+// Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
+alert('Boas Vindas ao nosso site');
+
+// Declare uma variável chamada nome e atribua a ela o valor "Lua".
+let nome = 'Lua';
+
+// Crie uma variável chamada idade e atribua a ela o valor 25.
+let idade = 25;
+
+// Defina uma variável numeroDeVendas e atribua a ela o valor 50.
+let numeroDeVendas = 50;
+
+// Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
+let saldoDisponivel = 1000;
+
+// Exiba um alerta com o texto "Erro! Preencha todos os campos"
+alert('Erro! Preencha todos os campos');
+
+// Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora 
+// exiba um alerta com o valor da variável mensagemDeErro.
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+
+// Use um novo prompt para perguntar o nome do usuário e armazená-lo em uma variável, 
+// pode chamá-la de nome ou adicionar o que desejar 
+let novoNome = prompt('Digite seu nome: ');
+
+// Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
+// Agora, para validar a idade que capturamos no desafio 09, caso a idade seja maior ou igual que 18, 
+// exiba um alerta com a mensagem "Pode tirar a habilitação!".
+let novaIdade = prompt('Digite sua idade: ')
+if (novaIdade >= 18) {
+    alert('Pode tirar a habilitação');
+}
+
--- doZero/logica-programacao/logica-js-projeto_inicial/app.js
+++ doZero/logica-programacao/logica-js-projeto_inicial/app.js
@@ -1,1 +1,8 @@
 alert('Bem Vindo ao Jogo do Número Secreto');
+let numeroSecreto = 5;
+let guess = prompt('Escolha um número entre 1 e 10');
+
+// Criando a comparação/condição
+if (guess == numeroSecreto) {
+    console.log('Você acertou');
+}
