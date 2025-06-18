9d91a55b30031d065f30d8920b6de3097ef02f83
Lucas <lucas.emanuel.carvalho@outlook.com>
2025-06-18 19:42:08 -0300

    Adicionando while

--- doZero/logica-programacao/logica-js-projeto_inicial/app.js
+++ doZero/logica-programacao/logica-js-projeto_inicial/app.js
@@ -1,14 +1,19 @@
 alert('Bem Vindo ao Jogo do Número Secreto');
 let numeroSecreto = 5;
-let guess = prompt('Escolha um número entre 1 e 10');
+let guess
 
-// Criando a comparação/condição
-if (guess == numeroSecreto) {
-    alert(`Você acertou. O numero secreto é ${numeroSecreto}`);
-} else {
-    if (guess > numeroSecreto) {
-        alert(`O numero secreto é menor que ${guess}`)
+// Criando laço enquanto (while)
+
+while (guess != numeroSecreto) {
+    guess = prompt('Escolha um numero entre 1 e 10');
+    // Criando a comparação/condição
+    if (guess == numeroSecreto) {
+        alert(`Você acertou. O numero secreto é ${numeroSecreto}`);
     } else {
-        alert(`O numero secreto é maior que ${guess}`)
+        if (guess > numeroSecreto) {
+            alert(`O numero secreto é menor que ${guess}`)
+        } else {
+            alert(`O numero secreto é maior que ${guess}`)
+        }
     }
 }
