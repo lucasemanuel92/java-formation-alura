55196285ea62729739bd49d3a13a0f588e815689
Lucas <lucas.emanuel.carvalho@outlook.com>
2025-07-14 17:50:09 -0300

    Parte 1

---
+++ doZero/logica-programacao-listas-func/js-curso-2/app.js
@@ -0,0 +1,7 @@
+// Alterar o valor selecionando um valor 
+let titulo = document.querySelector('h1');
+// Altera dentro do HTML o elemento com esse título
+titulo.innerHTML = 'Jogo do Número Secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 - 10';
